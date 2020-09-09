55f3c66daa44118afc374928fd1f766345ce867f
wangbenjun <wangbenjun@gmail.com>
2020-09-09 23:49:31 +0800

    speak

--- Php/practice/demo.php
+++ Php/practice/demo.php
@@ -6,3 +6,17 @@ $b = "world";
 echo $a, $b;
 echo $a, $b;
 echo $a, $b;
+
+echo "\n";
+
+echo "before do\n";
+doSomething();
+echo "after do\n";
+
+
+function doSomething()
+{
+    pow(2,10);
+    echo "doSomething\n";
+    return;
+}
